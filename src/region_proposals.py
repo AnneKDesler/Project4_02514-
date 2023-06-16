2fdae25aee2e61f155d0e7be9b618aa131c06b26
Anne Kjerstine Desler <s194333@dtu.dk>
2023-06-16 17:23:22 +0200

    region proposals and tools

--- src/region_proposals.py
+++ src/region_proposals.py
@@ -1,7 +1,9 @@
 import selectivesearch
 import cv2
+from src.load_data import WASTE, get_dataloaders_WASTE
+import numpy as np
 
-def SS(img):
+def SS(img, size=128):
     """
     Performs Selective Search on an image
     :param img: image to be processed
@@ -9,28 +11,33 @@ def SS(img):
              regions: regions of the image
     """
     img_lbl, regions = selectivesearch.selective_search(img)#, scale=500, sigma=0.9, min_size=10)
+    # crop regions out of the image and resize and append to rps
 
     return img_lbl, regions
 
 
 if __name__ == "__main__":
-    path = "/u/data/s194333/DLCV/Project4_02514-/data/batch_1/000000.jpg"
+    path = "/u/data/s194333/DLCV/Project4_02514-/data/batch_1/000001.jpg"
     img = cv2.imread(path)
-    # resize image to 512x512
     img = cv2.resize(img, (512, 512))
-    print(img.shape)
-    print(img.dtype)
-    print(img.max())
-    img_lbl, regions = SS(img)
-    print(regions)
-    print(len(regions))
-    print(regions[0])
 
-    # draw rectangles on the original image
+    img_lbl, regions = SS(img)
+    size = 128
+
+    rps = []
     for r in regions:
         x, y, w, h = r['rect']
-        cv2.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), 2)
+        if w == 0 or h == 0:
+            continue
+        crop = img[y:y+h, x:x+w]
+        crop = cv2.resize(crop, (size, size))
+        rps.append(crop)
     
-    cv2.imwrite("img.png", img)
+    # save the first 10 regions
+    for i, rp in enumerate(rps[:10]):
+        cv2.imwrite(f"region_proposals{i}.jpg", rp)
+
+
+
 
 
---
+++ src/tools.py
@@ -0,0 +1,172 @@
+import numpy as np
+import torch
+
+def xyxy2xywh(x):
+    # Convert bounding box format from [x1, y1, x2, y2] to [x, y, w, h]
+    y = torch.zeros_like(x) if isinstance(x, torch.Tensor) else np.zeros_like(x)
+
+    y[:, 0] = (x[:, 0] + x[:, 2]) / 2.0
+    y[:, 1] = (x[:, 1] + x[:, 3]) / 2.0
+    y[:, 2] = x[:, 2] - x[:, 0]
+    y[:, 3] = x[:, 3] - x[:, 1]
+    return y
+
+
+def xywh2xyxy(x):
+    # Convert bounding box format from [x, y, w, h] to [x1, y1, x2, y2]
+    y = torch.zeros_like(x) if isinstance(x, torch.Tensor) else np.zeros_like(x)
+    y[:, 0] = x[:, 0] - x[:, 2] / 2
+    y[:, 1] = x[:, 1] - x[:, 3] / 2
+    y[:, 2] = x[:, 0] + x[:, 2] / 2
+    y[:, 3] = x[:, 1] + x[:, 3] / 2
+    return y
+
+
+def bbox_iou(box1, box2, mode="xyxy"):
+    """
+    numpy version iou, and use for nms
+    """
+    # Get the coordinates of bounding boxes
+
+    if mode == "xyxy":
+        # x1, y1, x2, y2 = box1
+        b1_x1, b1_y1, b1_x2, b1_y2 = box1[..., 0], box1[..., 1], box1[..., 2], box1[..., 3]
+        b2_x1, b2_y1, b2_x2, b2_y2 = box2[..., 0], box2[..., 1], box2[..., 2], box2[..., 3]
+    else:
+        # x, y, w, h = box1
+        b1_x1, b1_x2 = box1[..., 0] - box1[..., 2] / 2, box1[..., 0] + box1[..., 2] / 2
+        b1_y1, b1_y2 = box1[..., 1] - box1[..., 3] / 2, box1[..., 1] + box1[..., 3] / 2
+        b2_x1, b2_x2 = box2[..., 0] - box2[..., 2] / 2, box2[..., 0] + box2[..., 2] / 2
+        b2_y1, b2_y2 = box2[..., 1] - box2[..., 3] / 2, box2[..., 1] + box2[..., 3] / 2
+
+    # Intersection area
+    inter_area = np.maximum((np.minimum(b1_x2, b2_x2) - np.maximum(b1_x1, b2_x1)), 0.0) * \
+                 np.maximum(np.minimum(b1_y2, b2_y2) - np.maximum(b1_y1, b2_y1), 0.0)
+
+    # Union Area
+    union_area = ((b1_x2 - b1_x1) * (b1_y2 - b1_y1) + 1e-16) + \
+                 (b2_x2 - b2_x1) * (b2_y2 - b2_y1) - inter_area
+
+    return inter_area / union_area  # iou
+
+
+def iou_xywh_numpy(boxes1, boxes2):
+    """
+    :param boxes1: boxes1和boxes2的shape可以不相同，但是需要满足广播机制
+    :param boxes2: 且需要保证最后一维为坐标维，以及坐标的存储结构为(x,y,w,h)，其中(x,y)是bbox的中心坐标
+    :return: 返回boxes1和boxes2的IOU，IOU的shape为boxes1和boxes2广播后的shape[:-1]
+    """
+    boxes1 = np.array(boxes1)
+    boxes2 = np.array(boxes2)
+
+    boxes1_area = boxes1[..., 2] * boxes1[..., 3]
+    boxes2_area = boxes2[..., 2] * boxes2[..., 3]
+
+    # 分别计算出boxes1和boxes2的左上角坐标、右下角坐标
+    # 存储结构为(xmin, ymin, xmax, ymax)，其中(xmin,ymin)是bbox的左上角坐标，(xmax,ymax)是bbox的右下角坐标
+    boxes1 = np.concatenate([boxes1[..., :2] - boxes1[..., 2:] * 0.5,
+                             boxes1[..., :2] + boxes1[..., 2:] * 0.5], axis=-1)
+    boxes2 = np.concatenate([boxes2[..., :2] - boxes2[..., 2:] * 0.5,
+                             boxes2[..., :2] + boxes2[..., 2:] * 0.5], axis=-1)
+
+    # 计算出boxes1与boxes1相交部分的左上角坐标、右下角坐标
+    left_up = np.maximum(boxes1[..., :2], boxes2[..., :2])
+    right_down = np.minimum(boxes1[..., 2:], boxes2[..., 2:])
+
+    # 因为两个boxes没有交集时，(right_down - left_up) < 0，所以maximum可以保证当两个boxes没有交集时，它们之间的iou为0
+    inter_section = np.maximum(right_down - left_up, 0.0)
+    inter_area = inter_section[..., 0] * inter_section[..., 1]
+    union_area = boxes1_area + boxes2_area - inter_area
+    IOU = 1.0 * inter_area / union_area
+    return IOU
+
+
+def iou_xyxy_numpy(boxes1, boxes2):
+
+    boxes1 = np.array(boxes1)
+    boxes2 = np.array(boxes2)
+
+    boxes1_area = (boxes1[..., 2] - boxes1[..., 0]) * (boxes1[..., 3] - boxes1[..., 1])
+    boxes2_area = (boxes2[..., 2] - boxes2[..., 0]) * (boxes2[..., 3] - boxes2[..., 1])
+
+    left_up = np.maximum(boxes1[..., :2], boxes2[..., :2])
+    right_down = np.minimum(boxes1[..., 2:], boxes2[..., 2:])
+    
+    inter_section = np.maximum(right_down - left_up, 0.0)
+    inter_area = inter_section[..., 0] * inter_section[..., 1]
+    union_area = boxes1_area + boxes2_area - inter_area
+    IOU = 1.0 * inter_area / union_area
+    return IOU
+
+
+def iou_xyxy_torch(boxes1, boxes2):
+    """
+    :param boxes1: boxes1和boxes2的shape可以不相同，但是需要满足广播机制，且需要是Tensor
+    :param boxes2: 且需要保证最后一维为坐标维，以及坐标的存储结构为(xmin, ymin, xmax, ymax)
+    :return: 返回boxes1和boxes2的IOU，IOU的shape为boxes1和boxes2广播后的shape[:-1]
+    """
+    boxes1_area = (boxes1[..., 2] - boxes1[..., 0]) * (boxes1[..., 3] - boxes1[..., 1])
+    boxes2_area = (boxes2[..., 2] - boxes2[..., 0]) * (boxes2[..., 3] - boxes2[..., 1])
+
+    # 计算出boxes1与boxes1相交部分的左上角坐标、右下角坐标
+    left_up = torch.max(boxes1[..., :2], boxes2[..., :2])
+    right_down = torch.min(boxes1[..., 2:], boxes2[..., 2:])
+
+    # 因为两个boxes没有交集时，(right_down - left_up) < 0，所以maximum可以保证当两个boxes没有交集时，它们之间的iou为0
+    inter_section = torch.max(right_down - left_up, torch.zeros_like(right_down))
+    inter_area = inter_section[..., 0] * inter_section[..., 1]
+    union_area = boxes1_area + boxes2_area - inter_area
+    IOU = 1.0 * inter_area / union_area
+    return IOU
+
+
+def iou_xywh_torch(boxes1, boxes2):
+    """
+    :param boxes1: boxes1和boxes2的shape可以不相同，但是需要满足广播机制，且需要是Tensor
+    :param boxes2: 且需要保证最后一维为坐标维，以及坐标的存储结构为(x, y, w, h)
+    :return: 返回boxes1和boxes2的IOU，IOU的shape为boxes1和boxes2广播后的shape[:-1]
+    """
+    boxes1_area = boxes1[..., 2] * boxes1[..., 3]
+    boxes2_area = boxes2[..., 2] * boxes2[..., 3]
+
+    # 分别计算出boxes1和boxes2的左上角坐标、右下角坐标
+    # 存储结构为(xmin, ymin, xmax, ymax)，其中(xmin,ymin)是bbox的左上角坐标，(xmax,ymax)是bbox的右下角坐标
+    boxes1 = torch.cat([boxes1[..., :2] - boxes1[..., 2:] * 0.5,
+                        boxes1[..., :2] + boxes1[..., 2:] * 0.5], dim=-1)
+    boxes2 = torch.cat([boxes2[..., :2] - boxes2[..., 2:] * 0.5,
+                        boxes2[..., :2] + boxes2[..., 2:] * 0.5], dim=-1)
+    # 计算出boxes1与boxes1相交部分的左上角坐标、右下角坐标
+    left_up = torch.max(boxes1[..., :2], boxes2[..., :2])
+    right_down = torch.min(boxes1[..., 2:], boxes2[..., 2:])
+    # 因为两个boxes没有交集时，(right_down - left_up) < 0，所以maximum可以保证当两个boxes没有交集时，它们之间的iou为0
+    inter_section = torch.max(right_down - left_up, torch.zeros_like(right_down))
+    inter_area = inter_section[..., 0] * inter_section[..., 1]
+    union_area = boxes1_area + boxes2_area - inter_area
+    IOU = 1.0 * inter_area / union_area
+
+    return IOU
+
+
+def nms(bboxes, score_threshold, iou_threshold, sigma=0.3, method='nms'):
+
+    classes_in_img = list(set(bboxes[:, 5].astype(np.int32)))
+    best_bboxes = []
+
+    for cls in classes_in_img:
+        cls_mask = (bboxes[:, 5].astype(np.int32) == cls)
+        cls_bboxes = bboxes[cls_mask]
+        while len(cls_bboxes) > 0:
+            max_ind = np.argmax(cls_bboxes[:, 4])
+            best_bbox = cls_bboxes[max_ind]
+            best_bboxes.append(best_bbox)
+            cls_bboxes = np.concatenate([cls_bboxes[: max_ind], cls_bboxes[max_ind + 1:]])
+            iou = iou_xyxy_numpy(best_bbox[np.newaxis, :4], cls_bboxes[:, :4])
+
+            weight = np.ones((len(iou),), dtype=np.float32)
+            iou_mask = iou > iou_threshold
+            weight[iou_mask] = 0.0
+            
+            cls_bboxes[:, 4] = cls_bboxes[:, 4] * weight
+            score_mask = cls_bboxes[:, 4] > score_threshold
+            cls_bboxes = cls_bboxes[score_mask]
+    return np.array(best_bboxes)
