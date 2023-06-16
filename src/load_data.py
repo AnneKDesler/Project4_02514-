a8013876bbb0ee1cb349dd20b39b944ec748fe14
Anne Kjerstine Desler <s194333@dtu.dk>
2023-06-16 15:30:15 +0200

    Data loader

--- src/load_data.py
+++ src/load_data.py
@@ -1,6 +1,7 @@
 import torch
 from torch.utils.data import DataLoader
 from torch.utils.data import random_split
+from torchvision import transforms
 import os
 import glob
 import numpy as np
@@ -8,70 +9,127 @@ import PIL.Image as Image
 import albumentations as A
 from albumentations.pytorch import ToTensorV2
 import cv2
+import json
+import pandas as pd
+import seaborn as sns; sns.set()
+from PIL import Image, ExifTags
+from pycocotools.coco import COCO
+from matplotlib.collections import PatchCollection
+import random
+import pylab
+import matplotlib.pyplot as plt
+from matplotlib.patches import Rectangle
 
-class DRIVE(torch.utils.data.Dataset):
-    def __init__(self, train, transform, data_path='/dtu/datasets1/02514/DRIVE/training'):
+import data_augment as dataAug
+
+class WASTE(torch.utils.data.Dataset):
+    def __init__(self, data_path='/dtu/datasets1/02514/data_wastedetection', img_size=512):
         'Initialization'
-        self.transform = transform
-        self.image_paths = sorted(glob.glob(data_path + '/images/*.tif'))
-        self.mask_paths = sorted(glob.glob(data_path + '/mask/*.gif'))
-        self.manual_paths = sorted(glob.glob(data_path + '/1st_manual/*.gif'))
+        self.img_size = img_size
+        self.path = data_path
+        self.ann_path = data_path + '/annotations.json'
+
+        with open(self.ann_path, 'r') as f:
+            dataset = json.loads(f.read())
+
+        categories = dataset['categories']
+        self.anns = dataset['annotations']
+        self.imgs = dataset['images']
+        self.img_ids = []
+        for img in self.imgs:
+            self.img_ids.append(img['id'])
+
+        cat_names = []
+        super_cat_names = []
+        super_cat_ids = {}
+        super_cat_last_name = ''
+        nr_super_cats = 0
+        for cat_it in categories:
+            cat_names.append(cat_it['name'])
+            super_cat_name = cat_it['supercategory']
+            # Adding new supercat
+            if super_cat_name != super_cat_last_name:
+                super_cat_names.append(super_cat_name)
+                super_cat_ids[super_cat_name] = nr_super_cats
+                super_cat_last_name = super_cat_name
+                nr_super_cats += 1
+
+        self.super_cat_ids = super_cat_ids
+
+
+
+        self.cat_ids_2_supercat_ids = {}
+        for cat in categories:
+            self.cat_ids_2_supercat_ids[cat['id']] = super_cat_ids[cat['supercategory']]
+
         
+    
     def __len__(self):
         'Returns the total number of samples'
-        return len(self.image_paths)
+        return len(self.img_ids)
+    
 
     def __getitem__(self, idx):
         'Generates one sample of data'
-        image_path = self.image_paths[idx]
-        mask_path = self.mask_paths[idx]
-        manual_path = self.manual_paths[idx]
-        
-        image = np.array(Image.open(image_path),dtype=np.uint8)
-        mask = np.array(Image.open(mask_path),dtype=np.uint8)
-        mask = mask//255
-        manual = np.array(Image.open(manual_path),dtype=np.uint8)
-        manual = manual//255
-        combined = np.concatenate((manual[...,None], mask[...,None]), axis=-1)
-        
-        transformed = self.transform(image=image, manual=manual, mask=mask)
-        X = transformed["image"]
-        Y = transformed["manual"]
-        Z = transformed["mask"]
-        return X, Y, Z
-    
-def get_dataloaders_DRIVE(batch_size, num_workers=8, seed=42, data_path="/dtu/datasets1/02514/DRIVE/training"):
 
-    data_transform_val = A.Compose(
-        [
-            A.PadIfNeeded(min_height=576, min_width=576),
-            A.CenterCrop(576, 576),
-            A.Normalize(mean=0.5, std=0.5),
-            ToTensorV2(),
-        ],
-        additional_targets={'manual': 'mask'}
-    )
-    data_transform_train = A.Compose(
-        [
-            A.PadIfNeeded(min_height=576, min_width=576),
-            A.CenterCrop(576, 576),
-            A.Normalize(mean=0.5, std=0.5),
-            A.Rotate(limit=45, border_mode=cv2.BORDER_CONSTANT, p=1.0),
-            A.VerticalFlip(p=0.5),
-            A.GridDistortion(border_mode=cv2.BORDER_CONSTANT, p=0.75),
-            A.ColorJitter(brightness=0.05, contrast=0.05, saturation=0.05, hue=0, p=1.0),
-            ToTensorV2(),
-        ],
-        additional_targets={'manual': 'mask'}
-    )
+        # Obtain Exif orientation tag code
+        for orientation in ExifTags.TAGS.keys():
+            if ExifTags.TAGS[orientation] == 'Orientation':
+                break
+
+        # Loads dataset as a coco object
+        coco = COCO(self.ann_path)
+
+        # Find image id
+        img_id = self.img_ids[idx]
+
+        img_path = self.imgs[img_id]['file_name']
+
+        I = Image.open(self.path + '/' + img_path)
+        # Load and process image metadata
+        if I._getexif():
+            exif = dict(I._getexif().items())
+            # Rotate portrait and upside down images if necessary
+            if orientation in exif:
+                if exif[orientation] == 3:
+                    I = I.rotate(180,expand=True)
+                if exif[orientation] == 6:
+                    I = I.rotate(270,expand=True)
+                if exif[orientation] == 8:
+                    I = I.rotate(90,expand=True)
+
+
+        annIds = coco.getAnnIds(imgIds=img_id, catIds=[], iscrowd=None)
+        anns_sel = coco.loadAnns(annIds)
+
+        target = []
     
-    trainset = DRIVE(train=True, transform=data_transform_train, data_path=data_path)
-    testset = DRIVE(train=False, transform=data_transform_val, data_path=data_path)
+        # Show annotations
+        for ann in anns_sel:
+            #[x, y, w, h]
+            bbox = ann['bbox']
+            cat = ann['category_id']
+            super_cat = self.cat_ids_2_supercat_ids[cat]
+            target.append([bbox[0], bbox[1], bbox[2], bbox[3], super_cat])
+
+
+        target = np.array(target)
+
+        I, target = dataAug.Resize((self.img_size, self.img_size), True)(np.copy(I), np.copy(target))
+        # normalize
+        X = torch.from_numpy(I).permute(2,0,1)
+        X = transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5])(X)
+
+        Y = torch.from_numpy(target)
+        return X, Y
+    
+def get_dataloaders_WASTE(batch_size, num_workers=8, seed=42, data_path='/dtu/datasets1/02514/data_wastedetection'):
+    
+    
+    dataset = WASTE(data_path=data_path)
 
     generator1 = torch.Generator().manual_seed(seed)
-    trainset, _, _ = random_split(trainset, [0.5, 0.25, 0.25], generator=generator1)
-    generator1 = torch.Generator().manual_seed(seed)
-    _, valset, testset = random_split(testset, [0.5, 0.25, 0.25], generator=generator1)
+    trainset, valset, testset = random_split(dataset, [0.8, 0.1, 0.1], generator=generator1)
 
     train_loader = DataLoader(trainset, batch_size=batch_size, shuffle=True, num_workers=num_workers)
     test_loader = DataLoader(testset, batch_size=batch_size, shuffle=False, num_workers=num_workers)
@@ -80,142 +138,26 @@ def get_dataloaders_DRIVE(batch_size, num_workers=8, seed=42, data_path="/dtu/da
     return train_loader, val_loader, test_loader
 
 
-class PH2(torch.utils.data.Dataset):
-    def __init__(self, set_type, transform, split=[60,20,20], seed=42, data_path="/dtu/datasets1/02514/PH2_Dataset_images"):
-        'Initialization'
-        self.transform = transform
-        allpaths = np.array(sorted(os.listdir(data_path)))
-        
-        split = np.array(split, dtype=np.float32)
-        split *= len(allpaths)/np.sum(split)
-        split = np.cumsum(split)
-        
-        np.random.seed(seed)
-        random_ordering = np.random.permutation(len(allpaths))
-        train_idx = random_ordering[:int(split[0])]
-        val_idx = random_ordering[int(split[0]):int(split[1])]
-        test_idx = random_ordering[int(split[1]):]
-        
-        if set_type.lower() == "train":
-            allpaths = allpaths[train_idx]
-        elif set_type.lower() in ["val", "validation"]:
-            allpaths = allpaths[val_idx]
-        elif set_type.lower() == "test":
-            allpaths = allpaths[test_idx]
-        else:
-            raise AttributeError
-        
-        self.label_paths = []
-        self.image_paths = []
-        
-        for i, name in enumerate(allpaths):
-            self.image_paths.append(os.path.join(data_path, name, name+"_Dermoscopic_Image", name+".bmp"))
-            self.label_paths.append(os.path.join(data_path, name, name+"_lesion", name+"_lesion.bmp"))
-        
-    def __len__(self):
-        'Returns the total number of samples'
-        return len(self.image_paths)
-
-    def __getitem__(self, idx):
-        'Generates one sample of data'
-        image_path = self.image_paths[idx]
-        mask_path = self.label_paths[idx]
-        
-        image = np.array(Image.open(image_path), dtype=np.uint8)
-        mask = np.array(Image.open(mask_path), dtype=np.uint8)
-        
-        transformed = self.transform(image=image, mask=mask)
-        X = transformed["image"]
-        Y = transformed["mask"]
-        return X, Y
-
-
-def get_dataloaders_PH2(batch_size, num_workers=8, seed=42, data_path="/dtu/datasets1/02514/PH2_Dataset_images"):
-    
-    data_transform_val = A.Compose(
-        [
-            A.Normalize(mean=0.5, std=0.5),
-            A.PadIfNeeded(min_height=576, min_width=576),
-            A.CenterCrop(576, 576),
-            ToTensorV2(),
-        ]
-    )
-    data_transform_train = A.Compose(
-        [
-            A.Normalize(mean=0.5, std=0.5),
-            A.PadIfNeeded(min_height=576, min_width=576),
-            A.CenterCrop(576, 576),
-            A.Rotate(limit=45, p=1.0),
-            A.VerticalFlip(p=0.5),
-            A.GridDistortion(p=0.75),
-            A.ColorJitter(brightness=0.05, contrast=0.05, saturation=0.05, hue=0, p=1.0),
-            ToTensorV2(),
-        ]
-    )
-    
-    trainset = PH2("train", data_transform_train, seed=seed, data_path=data_path)
-    valset = PH2("val", data_transform_val, seed=seed, data_path=data_path)
-    testset = PH2("test", data_transform_val, seed=seed, data_path=data_path)
-    
-    trainloader = DataLoader(
-        trainset, batch_size=batch_size, num_workers=num_workers, shuffle=True
-    )
-    valloader = DataLoader(
-        valset, batch_size=batch_size, num_workers=num_workers, shuffle=False
-    )
-    testloader = DataLoader(
-        testset, batch_size=batch_size, num_workers=num_workers, shuffle=False
-    )
-
-    return trainloader, valloader, testloader
-
-
 if __name__ == "__main__":
-    
-    torch.manual_seed(41)
-    trainloader,valloader,testloader = get_dataloaders_PH2(batch_size=1)
-    
-    print(f"Lengths of train, val, test are: {len(trainloader)}, {len(valloader)}, {len(testloader)}")
-    # for img, target in testloader:
-    #     # shape roughly 575x767
-    #     print(f"Img: {img.shape}, and target: {target.shape} ")
-    
-    import matplotlib.pyplot as plt
-    
-    img, target = list(trainloader)[0]
-    img, target = img.numpy().transpose((0,3,2,1)).squeeze(), target.numpy().transpose((2,1,0)).squeeze()
-    plt.figure()
-    plt.imshow(img)
-    plt.colorbar()
-    plt.savefig("Testing_PH2_img.png")
-    plt.show()
-    plt.figure()
-    plt.imshow(target)
-    plt.colorbar()
-    plt.savefig("Testing_PH2_target.png")
-    plt.show()
-    
-    train_loader, val_loader, test_loader = get_dataloaders_DRIVE(batch_size=1, seed=42)
-    
-    print('Loaded %d training images' % len(train_loader))
-    print('Loaded %d test images' % len(test_loader))
+    path = "/u/data/s194333/DLCV/Project4_02514-/data"
+    dataset = WASTE(data_path=path)
+    cats = dataset.super_cat_ids
 
-    # IMG Size  565x584
-    import matplotlib.pyplot as plt
-    img, target, mask = list(train_loader)[0]
-    img, target, mask = img.numpy().transpose((0,3,2,1)).squeeze(), target.numpy().transpose((2,1,0)).squeeze(), mask.numpy().transpose((2,1,0)).squeeze()
-    plt.figure()
-    plt.imshow(img)
-    plt.colorbar()
-    plt.savefig("Testing_DRIVE_img.png")
-    plt.show()
-    plt.figure()
-    plt.imshow(mask)
-    plt.colorbar()
-    plt.savefig("Testing_DRIVE_mask.png")
-    plt.show()
-    plt.figure()
-    plt.imshow(target.squeeze())
-    plt.colorbar()
-    plt.savefig("Testing_DRIVE_target.png")
-    plt.show()
+
+    train_loader, val_loader, test_loader = get_dataloaders_WASTE(batch_size=1, num_workers=0, seed=42, data_path=path)
+    img, target = next(iter(test_loader))
+
+    fig,ax = plt.subplots(1)
+    plt.axis('off')
+    ax.imshow(img.squeeze(0).permute(1,2,0))
+    for box in target[0]:
+        rect = Rectangle((box[0],box[1]),box[2],box[3],linewidth=1,edgecolor='r',facecolor='none')
+        ax.add_patch(rect)
+        # get keyword argument from super category id
+        label = list(cats.keys())[list(cats.values()).index(box[4].item())]
+        plt.text(box[0]+box[2],box[1],str(label),color='r')
+    fig.savefig('test.png', bbox_inches='tight', pad_inches=0)
+        
+
+
+
