6ce78b390928387f774e0da1c1bdc6619360ce05
luchsonice <s194369@student.dtu.dk>
2023-06-19 10:06:39 +0200

    model loss

--- src/model.py
+++ src/model.py
@@ -9,12 +9,6 @@ from sklearn.metrics import confusion_matrix
 from src.loss import binary_focal_loss_with_logits
 from torchvision.models import resnet50, ResNet50_Weights
 
-def bce_loss(pred, target):	
-    return torch.mean(F.binary_cross_entropy_with_logits(pred, target))
-
-def focal_loss(pred, target):	
-    return torch.mean(binary_focal_loss_with_logits(pred, target))
-
 class Model(pl.LightningModule):
     def __init__(
         self,
@@ -39,18 +33,10 @@ class Model(pl.LightningModule):
         # Freeze those weights
         for p in self.features.parameters():
             p.requires_grad = False
-
             
         self.lr = lr
         self.batch_size = batch_size
-        if loss == "focal":
-            self.loss = focal_loss
-        else:
-            if num_classes == 1:
-                # self.loss = BinaryFocalLossWithLogits(alpha=0.25, reduction="mean")
-                self.loss = bce_loss
-            else:
-                self.loss = torch.nn.CrossEntropyLoss()
+        self.loss = torch.nn.CrossEntropyLoss()
 
         if optimizer is None or optimizer == "Adam":
             self.optimizer = torch.optim.AdamW(
@@ -128,178 +114,6 @@ class Model(pl.LightningModule):
         self.log("test iou", iou, batch_size=self.batch_size, sync_dist=True)
         self.log("test dice", dice, batch_size=self.batch_size, sync_dist=True)
         self.log("test sensitivity", sensitivity, batch_size=self.batch_size, sync_dist=True)
-
-        return loss
-
-    def configure_optimizers(self) -> torch.optim.Optimizer:
-        return self.optimizer
-
-    def metrics(self, preds, target):
-        # Dice
-        X = target.view(-1)
-        Y = torch.sigmoid(preds.view(-1)) > 0.5
-
-        Y = Y*1.0
-        dice = 2*torch.mean(torch.mul(X,Y))/torch.mean(X+Y)
-
-        # Intersection over Union
-        IoU = torch.mean(torch.mul(X,Y))/(torch.mean(X+Y)-torch.mean(torch.mul(X,Y)))
-
-        X = X.cpu().numpy()
-        Y = Y.cpu().numpy()
-        tn, fp, fn, tp = confusion_matrix(X, Y).ravel()
-        accuracy = (tp+tn)/(tp+tn+fp+fn)		
-        
-        # Sensitivity
-        sensitivity = tp/(tp+fn)
-
-        # Specificity
-        specificity = tn/(tn+fp)
-
-        return dice, IoU, accuracy, sensitivity, specificity
-
-
-
-
-
-
-
-
-
-
-
-
-
-class DilatedNet(pl.LightningModule):
-    def __init__(
-        self,
-        num_classes: int = 1,
-        lr: Optional[float] = 1e-3,
-        weight_decay: Optional[float] = 0,
-        batch_size: Optional[int] = 1,
-        optimizer: Optional[str] = None,
-        target_mask_supplied: Optional[bool]=False,
-        loss = None,
-        *args,
-        **kwargs
-    ) -> None:
-        super(DilatedNet, self).__init__(*args, **kwargs)
-
-        self.target_mask_supplied = target_mask_supplied
-
-        # encoder (downsampling)
-        self.enc_conv0 = nn.Conv2d(3, 64, 3, dilation=1)
-        self.enc_conv1 = nn.Conv2d(64, 64, 3, dilation=2)
-        self.enc_conv2 = nn.Conv2d(64, 64, 3, dilation=4)
-        self.enc_conv3 = nn.Conv2d(64, 64, 3, dilation=8)
-
-        # bottleneck
-        self.bottleneck_conv = nn.Conv2d(64, 64, 3, padding=1)
-
-        # decoder (upsampling)
-        self.dec_conv0 = nn.ConvTranspose2d(64, 64, 3, dilation=8)
-        self.dec_conv1 = nn.ConvTranspose2d(64, 64, 3, dilation=4)
-        self.dec_conv2 = nn.ConvTranspose2d(64, 64, 3, dilation=2)
-        self.dec_conv3 = nn.ConvTranspose2d(64, 1, 3, dilation=1)
-
-        self.lr = lr
-        self.batch_size = batch_size
-        if loss == "focal":
-            self.loss = focal_loss
-        else:
-            if num_classes == 1:
-                # self.loss = BinaryFocalLossWithLogits(alpha=0.25, reduction="mean")
-                self.loss = bce_loss
-            else:
-                self.loss = torch.nn.CrossEntropyLoss()
-
-        if optimizer is None or optimizer == "Adam":
-            self.optimizer = torch.optim.Adam(
-                self.parameters(), lr=self.lr, weight_decay=weight_decay
-            )
-        if optimizer == "SGD":
-            self.optimizer = torch.optim.SGD(self.parameters(), lr=self.lr)
-
-    def forward(self, x):
-        # encoder
-        e0 = F.relu(self.enc_conv0(x))
-        e1 = F.relu(self.enc_conv1(e0))
-        e2 = F.relu(self.enc_conv2(e1))
-        e3 = F.relu(self.enc_conv3(e2))
-
-        # bottleneck
-        b = F.relu(self.bottleneck_conv(e3))
-
-        # decoder
-        d0 = F.relu(self.dec_conv0(b))
-        d1 = F.relu(self.dec_conv1(d0))
-        d2 = F.relu(self.dec_conv2(d1))
-        d3 = self.dec_conv3(d2)
-        return d3
-
-    def _inference_training(
-        self, batch, batch_idx: Optional[int] = None
-    ) -> torch.Tensor:
-        """
-        From https://huggingface.co/docs/transformers/model_doc/t5#training
-        """
-        if self.target_mask_supplied:
-            data, target, mask = batch
-        else:
-            data, target = batch
-        output = self(data)
-        if self.target_mask_supplied:
-            output *= mask[:,None,:,:]
-
-        output, target = output, target
-        #print(output.shape, target.shape)
-        output = output[:,0,:,:]
-        target = target.to(torch.float32)
-        dice, iou, accuracy, sensitivity, specificity = self.metrics(output, target)
-
-        #out_img = wandb.Image(
-        #    output[0,...].cpu().detach().numpy().squeeze(), 
-        #    caption="Prediction"
-        #)
-        #out_target = wandb.Image(
-        #    target[0,...].cpu().detach().numpy().squeeze(), 
-        #    caption="target"
-        #)
-        #self.logger.experiment.log({"prediction": [out_img, out_target]}) #, step = self.logger.experiment.current_trainer_global_step
-
-        return self.loss(output, target), accuracy, specificity, iou, dice, sensitivity
-
-    def training_step(
-        self, batch: List[str], batch_idx: Optional[int] = None
-    ) -> torch.Tensor:
-        loss, accuracy, specificity, iou, dice, sensitivity = self._inference_training(batch, batch_idx)
-        self.log("train loss", loss, batch_size=self.batch_size)
-        self.log("train accuracy", accuracy, batch_size=self.batch_size)
-        self.log("train specificity", specificity, batch_size=self.batch_size)
-        self.log("train iou", iou, batch_size=self.batch_size)
-        return loss
-
-    def validation_step(
-        self, batch: List[str], batch_idx: Optional[int] = None
-    ) -> torch.Tensor:
-        loss, accuracy, specificity, iou, dice, sensitivity = self._inference_training(batch, batch_idx)
-        self.log("val loss", loss, batch_size=self.batch_size, sync_dist=True)
-        self.log("val accuracy", accuracy, batch_size=self.batch_size, sync_dist=True)
-        self.log("val specificity", specificity, batch_size=self.batch_size, sync_dist=True)
-        self.log("val iou", iou, batch_size=self.batch_size, sync_dist=True)
-        return loss
-
-    def test_step(
-        self, batch: List[str], batch_idx: Optional[int] = None
-    ) -> torch.Tensor:
-        loss, accuracy, specificity, iou, dice, sensitivity = self._inference_training(batch, batch_idx)
-        self.log("test loss", loss, batch_size=self.batch_size, sync_dist=True)
-        self.log("test accuracy", accuracy, batch_size=self.batch_size, sync_dist=True)
-        self.log("test iou", iou, batch_size=self.batch_size, sync_dist=True)
-        self.log("test specificity", specificity, batch_size=self.batch_size, sync_dist=True)
-        self.log("test dice", dice, batch_size=self.batch_size, sync_dist=True)
-        self.log("test sensitivity", sensitivity, batch_size=self.batch_size, sync_dist=True)
-
 
         return loss
 
